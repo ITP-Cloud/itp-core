28cd88ac8317752d033caa506994c860636679b3
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2023-08-16 07:27:03 +0300

    feat: added shield auth

---
+++ app/Models/UserModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+use CodeIgniter\Shield\Models\UserModel as ModelsUserModel;
+
+class UserModel extends ModelsUserModel
+{
+    protected $allowedFields    = [
+        'username',
+        'firstname',
+        'lastname',
+        'phone',
+        'country',
+        'student_id',
+        'student_id_document',
+        'portrait_holding_id',
+        'institution',
+        'account_status'
+    ];
+}
