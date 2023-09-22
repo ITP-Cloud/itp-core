e307b2720c0f33fe702e978d350843e570aeedae
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2023-09-22 04:03:42 +0300

    update: added home and about logic

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,36 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\Shield\Entities\User;
+
 class Home extends BaseController
 {
+    private $data;
+
+    public function __construct()
+    {
+        $this->data = [];
+    }
+
     public function index(): string
     {
-        return view('welcome_message');
+        $this->data['page'] = 'home';
+        $this->data['title'] = 'ITP Cloud | Official Homepage';
+        $this->data['description'] = 'an open source miniature cloud platform for students by students 😏';
+
+        return view('templates/header', $this->data) .
+            view('index') .
+            view('templates/footer');
+    }
+
+    public function about(): string
+    {
+        $this->data['page'] = 'home';
+        $this->data['title'] = 'About | ITP Cloud';
+        $this->data['description'] = 'an open source miniature cloud platform for students by students 😏';
+
+        return view('templates/header', $this->data) .
+            view('index') .
+            view('templates/footer');
     }
 }
