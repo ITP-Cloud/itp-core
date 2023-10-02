a786c7d1dd2ef378e5c96a57ee55be85f9b4eed4
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2023-10-02 10:43:50 +0300

    feat: added email account activation

---
+++ app/Views/auth/emails/email_activate_email.php
@@ -0,0 +1,48 @@
+<style>
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+  }
+</style>
+<div style="font-family:Arial,Helvetica,sans-serif; line-height: 1.5; font-weight: normal; font-size: 15px; color: #2F3044; min-height: 100%; margin:0; padding:0; width:100%; background-color:#edf2f7">
+  <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse;margin:0 auto; padding:0; max-width:600px">
+    <tbody>
+      <tr>
+        <td align="center" valign="center" style="text-align:center; padding: 40px">
+          <a href="<?= base_url() ?>" rel="noopener" target="_blank">
+            <img alt="Logo" src="<?= base_url() ?>/assets/images/logos/logo.png" />
+          </a>
+        </td>
+      </tr>
+      <tr>
+        <td align="left" valign="center">
+          <div style="text-align:left; margin: 0 20px; padding: 40px; background-color:#ffffff; border-radius: 6px">
+            <!--begin:Email content-->
+            <div style="padding-bottom: 30px; font-size: 17px;">
+              <strong>Welcome to ITP Cloud 😃</strong>
+            </div>
+            <div style="padding-bottom: 30px">To activate your account, please use the activation code below to verify your email address.</div>
+            <div style="padding-bottom: 40px; text-align:center;">
+              <h3>Activation Code • <?= $code ?></h3>
+            </div>
+
+            <div style="padding-bottom: 30px">If you did not request a login link, no further action is required. You can safely disregard this email. </div>
+            <!--end:Email content-->
+            <div style="padding-bottom: 10px">Kind regards,
+              <br>The ITP Cloud Team 😎
+      <tr>
+        <td align="center" valign="center" style="font-size: 13px; text-align:center;padding: 20px; color: #6d6e7c;">
+          <p>Kitwe, Zambia.</p>
+          <p>Copyright &copy;
+            <a href="<?= base_url() ?>" rel="noopener" target="_blank">ITP Cloud</a>.
+          </p>
+        </td>
+      </tr></br>
+</div>
+</div>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
