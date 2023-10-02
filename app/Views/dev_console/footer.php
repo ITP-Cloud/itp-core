689c23e4073047dbda01350fc6dac3958190585a
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2023-10-02 09:37:14 +0300

    update: added clipboard lib

--- app/Views/dev_console/footer.php
+++ app/Views/dev_console/footer.php
@@ -8,9 +8,44 @@
 <script src="<?= base_url() ?>/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <script src="<?= base_url() ?>/assets/js/sidebarmenu.js"></script>
 <script src="<?= base_url() ?>/assets/js/app.min.js"></script>
-<script src="<?= base_url() ?>/assets/libs/apexcharts/dist/apexcharts.min.js"></script>
 <script src="<?= base_url() ?>/assets/libs/simplebar/dist/simplebar.js"></script>
-<script src="<?= base_url() ?>/assets/js/dashboard.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/clipboard@2.0.11/dist/clipboard.min.js"></script>
+<script>
+  const serverClipboard = new ClipboardJS('#clipboard-server-address');
+  const copyServerAddrBtn = document.getElementById('clipboard-server-address');
+
+  serverClipboard.on('success', function(e) {
+    console.log(e)
+    copyServerAddrBtn.innerHTML = '<i class="ti ti-clipboard-check fs-6"></i>';
+    setTimeout(() => {
+      copyServerAddrBtn.innerHTML = '<i class="ti ti-clipboard fs-6"></i>';
+    }, 1500);
+  });
+
+
+  const usernameClipboard = new ClipboardJS('#clipboard-username');
+  const copyUsernameBtn = document.getElementById('clipboard-username');
+
+  usernameClipboard.on('success', function(e) {
+    console.log(e)
+    copyUsernameBtn.innerHTML = '<i class="ti ti-clipboard-check fs-6"></i>';
+    setTimeout(() => {
+      copyUsernameBtn.innerHTML = '<i class="ti ti-clipboard fs-6"></i>';
+    }, 1500);
+  });
+
+
+  const passwordClipboard = new ClipboardJS('#clipboard-password');
+  const copyPasswordBtn = document.getElementById('clipboard-password');
+
+  passwordClipboard.on('success', function(e) {
+    console.log(e)
+    copyPasswordBtn.innerHTML = '<i class="ti ti-clipboard-check fs-6"></i>';
+    setTimeout(() => {
+      copyPasswordBtn.innerHTML = '<i class="ti ti-clipboard fs-6"></i>';
+    }, 1500);
+  });
+</script>
 </body>
 
 </html>
