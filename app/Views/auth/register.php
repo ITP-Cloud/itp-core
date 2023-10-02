42d775e3acc3fb1d105671628ed8c106a82b0545
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2023-10-02 10:17:40 +0300

    update: made some changes to register page

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -7,6 +7,7 @@
   <title>ITP Cloud | Register</title>
   <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url() ?>/favicon.png">
   <link rel="stylesheet" href="<?= base_url() ?>/assets/css/styles.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
@@ -35,31 +36,64 @@
 
             <div class="m-4 w-75 mx-auto">
               <a href=" <?= base_url() ?>" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                <img src="<?= base_url() ?>/assets/images/logos/logo.png" width="120" alt="">
-                <h2 class="mt-3">ITP Cloud</h2>
+                <img src="<?= base_url() ?>/assets/images/logos/dark-logo.svg" width="180" alt="" />
               </a>
               <!-- <p class="text-center">Buid Your Imagination</p> -->
-              <h3 class="text-center"><span class="text-primary">Register</span> an Account</h3>
+              <h4 class="text-center"><span class="text-primary">Register</span> an Account</h4>
+
+              <?php if (session('error') !== null) : ?>
+                <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
+              <?php elseif (session('errors') !== null) : ?>
+                <div class="alert alert-danger" role="alert">
+                  <?php if (is_array(session('errors'))) : ?>
+                    <?php foreach (session('errors') as $error) : ?>
+                      <?= $error ?>
+                      <br>
+                    <?php endforeach ?>
+                  <?php else : ?>
+                    <?= session('errors') ?>
+                  <?php endif ?>
+                </div>
+              <?php endif ?>
 
               <?= form_open('register', 'class="mt-4 needs-validation" novalidate') ?>
 
               <div class="mb-3">
-                <label for="">Full Names</label>
+                <label for="" class="form-label">Full Names</label>
                 <div class="input-group mb-3">
-                  <input type="text" class="form-control" placeholder="First Name" aria-label="First Name">
-                  <input type="text" class="form-control" placeholder="Last Name" aria-label="Last Name">
+                  <input type="text" class="form-control" placeholder="First Name" name="firstname" value="<?= set_value('firstname') ?>" aria-label="First Name">
+                  <input type="text" class="form-control" placeholder="Last Name" name="lastname" value="<?= set_value('lastname') ?>" aria-label="Last Name">
                 </div>
               </div>
+
               <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email</label>
-                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <label class="form-label">Email</label>
+                <input type="email" name="email" class="form-control" value="<?= set_value('email') ?>" aria-describedby="emailHelp">
+              </div>
+
+              <div class="mb-3">
+                <label class="form-label">Phone</label>
+                <input type="tel" name="phone" class="form-control" value="<?= set_value('phone') ?>" aria-describedby="phoneHelp">
               </div>
 
               <div class="mb-4">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                <label class="form-label">Password</label>
+
+                <div class="input-group mb-4">
+                  <input type="password" name="password" class="form-control">
+                  <span class="input-group-text"><i class="bi bi-eye"></i></span>
+                </div>
               </div>
 
+
+              <div class="mb-4">
+                <label class="form-label">Password</label>
+
+                <div class="input-group mb-4">
+                  <input type="password" name="password_confirm" class="form-control">
+                  <span class="input-group-text"><i class="bi bi-eye"></i></span>
+                </div>
+              </div>
 
               <button class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Create Account</button>
               <div class="d-flex align-items-center justify-content-center">
