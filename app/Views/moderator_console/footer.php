28daf63853287b089d0d43fb793417df310f4d39
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2023-10-04 09:20:35 +0300

    update: added header and footer for moder. console

---
+++ app/Views/moderator_console/footer.php
@@ -0,0 +1,15 @@
+<div class="py-6 px-6 text-center">
+  <p class="mb-0 fs-4">Copyright &copy; <?= date('Y') ?> ITP Cloud. All rights reserved.</p>
+</div>
+</div>
+</div>
+</div>
+<script src="<?= base_url() ?>/assets/libs/jquery/dist/jquery.min.js"></script>
+<script src="<?= base_url() ?>/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url() ?>/assets/js/sidebarmenu.js"></script>
+<script src="<?= base_url() ?>/assets/js/app.min.js"></script>
+<script src="<?= base_url() ?>/assets/libs/simplebar/dist/simplebar.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/clipboard@2.0.11/dist/clipboard.min.js"></script>
+</body>
+
+</html>
