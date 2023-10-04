cfbb42b98d0e419f3f3ead080e96e96359c7c036
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2023-10-04 09:33:43 +0300

    update: initial controller logic for moderator

--- app/Controllers/ModeratorConsole/ModeratorDashboardController.php
+++ app/Controllers/ModeratorConsole/ModeratorDashboardController.php
@@ -6,28 +6,65 @@ use App\Controllers\BaseController;
 
 class ModeratorDashboardController extends BaseController
 {
+    private $data;
+
+    public function __construct()
+    {
+        $this->data = [];
+    }
+
     public function index()
     {
-        //
+        $this->data['page'] = 'moderator_console';
+        $this->data['title'] = 'Moderator Console | ITP Cloud';
+        $this->data['description'] = 'an open source miniature cloud platform for students by students 😏';
+
+        return view('moderator_console/header', $this->data) .
+            view('moderator_console/index') .
+            view('moderator_console/footer');
     }
 
     public function getWebsites()
     {
-        //
+        $this->data['page'] = 'websites';
+        $this->data['title'] = 'Moderator Console | ITP Cloud';
+        $this->data['description'] = 'an open source miniature cloud platform for students by students 😏';
+
+        return view('moderator_console/header', $this->data) .
+            view('moderator_console/websites') .
+            view('moderator_console/footer');
     }
 
     public function getWebsite(int $website_id = 0)
     {
-        //
+        $this->data['page'] = 'websites';
+        $this->data['title'] = 'Moderator Console | ITP Cloud';
+        $this->data['description'] = 'an open source miniature cloud platform for students by students 😏';
+
+        return view('moderator_console/header', $this->data) .
+            view('moderator_console/website') .
+            view('moderator_console/footer');
     }
 
     public function getDatabases()
     {
-        //
+        $this->data['page'] = 'databases';
+        $this->data['title'] = 'Moderator Console | ITP Cloud';
+        $this->data['description'] = 'an open source miniature cloud platform for students by students 😏';
+
+        return view('moderator_console/header', $this->data) .
+            view('moderator_console/databases') .
+            view('moderator_console/footer');
     }
 
     public function getDatabase(int $database_id = 0)
     {
-        //
+        $this->data['page'] = 'databases';
+        $this->data['title'] = 'Moderator Console | ITP Cloud';
+        $this->data['description'] = 'an open source miniature cloud platform for students by students 😏';
+
+        return view('moderator_console/header', $this->data) .
+            view('moderator_console/database') .
+            view('moderator_console/footer');
     }
 }
--- app/Controllers/ModeratorConsole/UserManagementController.php
+++ app/Controllers/ModeratorConsole/UserManagementController.php
@@ -3,28 +3,63 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 
 class UserManagementController extends BaseController
 {
+    private $data;
+
+    public function __construct()
+    {
+        $this->data = [];
+    }
 
     public function getUsers()
     {
-        //
+        $this->data['page'] = 'moderator_console';
+        $this->data['title'] = 'Moderator Console | ITP Cloud';
+        $this->data['description'] = 'an open source miniature cloud platform for students by students 😏';
+        $this->data['users'] = (new UserModel())->findAll();
+
+        return view('moderator_console/header', $this->data) .
+            view('moderator_console/user_management/users') .
+            view('moderator_console/footer');
     }
 
     public function getUser(int $user_id = 0)
     {
-        //
+        $this->data['page'] = 'moderator_console';
+        $this->data['title'] = 'Moderator Console | ITP Cloud';
+        $this->data['description'] = 'an open source miniature cloud platform for students by students 😏';
+        $this->data['user'] = (new UserModel())->find($user_id);
+
+        return view('moderator_console/header', $this->data) .
+            view('moderator_console/user_management/user') .
+            view('moderator_console/footer');
     }
 
-    public function getKyc()
+    public function getKycApprovals()
     {
-        //
+        $this->data['page'] = 'moderator_console';
+        $this->data['title'] = 'Moderator Console | ITP Cloud';
+        $this->data['description'] = 'an open source miniature cloud platform for students by students 😏';
+        $this->data['users'] = (new UserModel())->where('account_status', 'under_review')->findAll();
+
+        return view('moderator_console/header', $this->data) .
+            view('moderator_console/user_management/kyc_approvals') .
+            view('moderator_console/footer');
     }
 
-    public function getPendingKycUserDetails(int $user_id = 0)
+    public function getKycUserDetails(int $user_id = 0)
     {
-        //
+        $this->data['page'] = 'moderator_console';
+        $this->data['title'] = 'Moderator Console | ITP Cloud';
+        $this->data['description'] = 'an open source miniature cloud platform for students by students 😏';
+        $this->data['user'] = (new UserModel())->where('account_status', 'under_review')->find($user_id);
+
+        return view('moderator_console/header', $this->data) .
+            view('moderator_console/user_management/kyc_candidate') .
+            view('moderator_console/footer');
     }
 
     public function approveUser(int $user_id = 0)
