dcd8d670beb142deaca557b0b9ba8c5699ce71ba
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2023-10-10 10:35:24 +0300

    feat: added method to generate secure user payload

--- app/Controllers/ModeratorConsole/UserManagementController.php
+++ app/Controllers/ModeratorConsole/UserManagementController.php
@@ -4,6 +4,7 @@ namespace App\Controllers\ModeratorConsole;
 
 use App\Controllers\BaseController;
 use App\Models\UserModel;
+use ReallySimpleJWT\Token;
 
 class UserManagementController extends BaseController
 {
@@ -62,13 +63,115 @@ class UserManagementController extends BaseController
             view('moderator_console/footer');
     }
 
-    public function approveUser(int $user_id = 0)
+    public function approveUser()
     {
-        //
+        if ($this->request->getPost()) {
+            $user_id = (int) $this->request->getPost('user_id');
+
+            $secureUserPayload = $this->generateSecureUserPayloadForUser($user_id);
+
+            $users = auth()->getProvider();
+            $user = $users->findById($user_id);
+            $user->username = $secureUserPayload;
+            $users->save($user);
+        } else {
+            return redirect()->back();
+        }
     }
 
-    public function rejectUser(int $user_id = 0)
+    public function declineUserAndRequestResubmission()
     {
-        //
+        if ($this->request->getPost()) {
+            $credentials = [
+                'email'    => auth()->user()->email,
+                'password' => $this->request->getPost('password')
+            ];
+
+            $validCreds = auth()->check($credentials);
+
+            if (!$validCreds->isOK()) {
+                return redirect()->back()->with('error', $validCreds->reason());
+            }
+
+
+            // Reject User Registration
+            $users = auth()->getProvider();
+
+            $user = $users->findById($this->request->getPost('user_id'));
+            $user->fill([
+                'account_status' => 'pending',
+                'student_id' => '',
+                'student_id_document' => '',
+                'institution' => '',
+                'country' => '',
+                'avatar' => '',
+            ]);
+            $users->save($user);
+
+            unlink('../public/assets/uploads/' . auth()->user()->student_id_doc);
+            unlink('../public/assets/uploads/' . auth()->user()->avatar);
+
+
+            // Notify user about action
+            $email = \Config\Services::email();
+
+            $email->setFrom('itp@gulanistores.com', 'ITP Cloud Moderator');
+            $email->setTo($user->email);
+
+            $email->setSubject('KYC Failure');
+            $email->setMessage(view('auth/emails/kyc_failure'));
+
+            $email->send();
+        }
+    }
+
+    private function generateSecurePassword()
+    {
+        $password = '';
+        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$^()_-=+;:,.?';
+
+        for ($i = 0; $i < 16; $i++) {
+            $password .= $chars[rand(0, strlen($chars) - 1)];
+        }
+
+        return $password;
+    }
+
+    private function generateSecureUserPayloadForUser(int $user_id = 0): string
+    {
+        $linux_username = 'itp_user_' . $user_id;
+        $linux_password = $this->generateSecurePassword();
+
+        $ftp_username = $linux_username;
+        $ftp_password = $linux_password;
+
+        $db_username = $linux_username;
+        $db_password = $this->generateSecurePassword();
+
+        $file_browser_username = $linux_username;
+        $file_browser_password = $this->generateSecurePassword();
+        $file_browser_base_path = '/home/' . $linux_username . '/ftp/websites';
+
+
+        $user_payload = [
+            'linuxUsername' => $linux_username,
+            'linuxPassword' => $linux_password,
+            'ftpUsername' => $ftp_username,
+            'ftpPassword' => $ftp_password,
+            'dbUsername' => $db_username,
+            'dbPassword' => $db_password,
+            'fileBrowserUsername' => $file_browser_username,
+            'fileBrowserPassword' => $file_browser_password,
+            'fileBrowserPort' => '',
+            'fileBrowserBasePath' => $file_browser_base_path
+        ];
+
+        $secret = env('userTokenSecret');
+        $token = Token::customPayload($user_payload, $secret);
+
+        // echo $token;
+        // echo '<br>';
+        // dd(Token::getPayload($token));
+        return $token;
     }
 }
