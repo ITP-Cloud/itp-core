7ccd35a00c44030af4c114b429d63de01b1c8d31
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2023-10-18 14:03:34 +0300

    update: added data fetching in controller

--- app/Controllers/ModeratorConsole/ModeratorDashboardController.php
+++ app/Controllers/ModeratorConsole/ModeratorDashboardController.php
@@ -3,14 +3,20 @@
 namespace App\Controllers\ModeratorConsole;
 
 use App\Controllers\BaseController;
+use App\Models\ManagedDatabaseModel;
+use App\Models\ManagedWebsiteModel;
 
 class ModeratorDashboardController extends BaseController
 {
-    private $data;
+    private array $data;
+    private ManagedWebsiteModel $websiteModel;
+    private ManagedDatabaseModel $databaseModel;
 
     public function __construct()
     {
         $this->data = [];
+        $this->websiteModel = new ManagedWebsiteModel();
+        $this->databaseModel = new ManagedDatabaseModel();
     }
 
     public function index()
@@ -29,42 +35,23 @@ class ModeratorDashboardController extends BaseController
         $this->data['page'] = 'websites';
         $this->data['title'] = 'Moderator Console | ITP Cloud';
         $this->data['description'] = 'an open source miniature cloud platform for students by students 😏';
+        $this->data['websites'] = $this->websiteModel->getAllWebsites();
 
         return view('moderator_console/header', $this->data) .
             view('moderator_console/websites') .
             view('moderator_console/footer');
     }
 
-    public function getWebsite(int $website_id = 0)
-    {
-        $this->data['page'] = 'websites';
-        $this->data['title'] = 'Moderator Console | ITP Cloud';
-        $this->data['description'] = 'an open source miniature cloud platform for students by students 😏';
-
-        return view('moderator_console/header', $this->data) .
-            view('moderator_console/website') .
-            view('moderator_console/footer');
-    }
 
     public function getDatabases()
     {
         $this->data['page'] = 'databases';
         $this->data['title'] = 'Moderator Console | ITP Cloud';
         $this->data['description'] = 'an open source miniature cloud platform for students by students 😏';
+        $this->data['databases'] = $this->databaseModel->getAllDatabases();
 
         return view('moderator_console/header', $this->data) .
             view('moderator_console/databases') .
-            view('moderator_console/footer');
-    }
-
-    public function getDatabase(int $database_id = 0)
-    {
-        $this->data['page'] = 'databases';
-        $this->data['title'] = 'Moderator Console | ITP Cloud';
-        $this->data['description'] = 'an open source miniature cloud platform for students by students 😏';
-
-        return view('moderator_console/header', $this->data) .
-            view('moderator_console/database') .
             view('moderator_console/footer');
     }
 }
