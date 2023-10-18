da9d9b5b52e56fd5f3bf6d6a1c3f9545f40f11c6
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2023-10-18 14:03:17 +0300

    update: partially implemented databases page

--- app/Views/moderator_console/databases.php
+++ app/Views/moderator_console/databases.php
@@ -2,7 +2,7 @@
   <div class="d-flex">
     <h2>Database <span class="text-primary">Management</span></h2>
 
-    <a href="http://192.168.239.129/phpmyadmin/" target="_blank" class="btn btn-outline-primary m-1 ms-auto">
+    <a href="<?= env('serverUrl') ?>/phpmyadmin/" target="_blank" class="btn btn-outline-primary m-1 ms-auto">
       Open phpMyAdmin
     </a>
 
@@ -21,7 +21,6 @@
           <button type="button" class="btn btn-primary w-100">Create Database</button>
         </div>
 
-
         <div class="mt-5">
           <div class="text-center">
             <div class="spinner-border text-primary" role="status">
@@ -36,34 +35,33 @@
   <div>
     <p class="text-muted">Manage all <span class="text-primary">databases</span> here.</p>
   </div>
-  <div class="row mt-7 justify-content-center">
+  <div class="row mt-7 justify-content-center gap-3">
     <?php $list = [1, 2, 3, 4, 5, 6]; ?>
-    <?php foreach ($list as $li) : ?>
+    <?php foreach ($databases as $database) : ?>
       <div class="col-auto">
-        <div class="card" style="width: 270px">
+        <div class="card" style="width: 200px">
           <div class="card-header">
-            MDB <?= $li ?>
+            <?= $$database['md_db_name'] ?>
           </div>
           <div class="text-center card-body">
             <div class="mb-4">
               <i class="ti ti-database" style="font-size: 60px;"></i>
             </div>
-            <p class="card-text">Database Name • mydb</p>
+            <p class="card-text">Database Name • <?= $$database['md_db_name'] ?></p>
 
             <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#getCredentialsCanvas<?= $li ?>" aria-controls="staticBackdrop">
-              Get Credentials
+              View Details
             </button>
-
-            <div class="offcanvas offcanvas-end" data-bs-backdrop="static" tabindex="-1" id="getCredentialsCanvas<?= $li ?>" aria-labelledby="staticBackdropLabel">
-              <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="staticBackdropLabel"> MDB <?= $li ?> • <span class="text-primary">Credentials</span></h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-              </div>
-              <div class="offcanvas-body">
-                <div>
-                  I will not close if you click outside of me.
-                </div>
-              </div>
+          </div>
+        </div>
+        <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="getCredentialsCanvas<?= $li ?>" aria-labelledby="staticBackdropLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="staticBackdropLabel"><span class="text-primary">Database</span> Details</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <div>
+              I will not close if you click outside of me.
             </div>
           </div>
         </div>
